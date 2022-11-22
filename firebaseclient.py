5ef08c101868675ccfa1abee9e790b38b9576734
Juan José Salazar Cortés <jjsc2003@live.com>
2022-11-22 13:34:49 +0000

    Practica Sensores y Python

---
+++ firebaseclient.py
@@ -0,0 +1,16 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account.
+cred = credentials.Certificate('credentials.json')
+
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+sensor_1_ref = db.collection(u'sensor_1')
+doc = sensor_1_ref.stream()
+
+for doc in docs:
+    print(f'{doc.id} => {doc.to_dict()}')
